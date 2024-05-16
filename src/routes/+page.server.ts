78a0ec1368b2ddf5defc7e744e6c8a94cbd7aee0
Oscar Saraza <oscarsaraza@gmail.com>
2024-05-16 16:42:31 -0500

    Implementación inicial de cómputo de calificación de rendimiento para Juzgado Promiscuo Municipal.

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,221 @@
+import type { PageServerLoad } from './$types';
+import xlsx from 'node-xlsx';
+import z from 'zod';
+import _ from 'lodash';
+import dayjs from 'dayjs';
+
+export const load = (async () => {
+	return {};
+}) satisfies PageServerLoad;
+
+// ['Movimiento de Tutelas', 'Funcionario: FERNANDO IBAGUE  PINILLA Cédula: 7321266', '01/JAN/2023', '31/MAR/2023', 0, 0, 0, 0, 0, 0, undefined, undefined, 10]
+const consolidadoRowSchema = z.tuple([
+	z.string(),
+	z.string(),
+	z.coerce.date(),
+	z.coerce.date(),
+	z.number(),
+	z.number(),
+	z.number(),
+	z.number(),
+	z.number(),
+	z.number(),
+	z.undefined(),
+	z.undefined(),
+	z.number()
+]);
+
+const consolidadoExtractedDataSchemaColumns = [
+	'categoria',
+	'funcionario',
+	'desde',
+	'hasta',
+	'inventarioInicial',
+	'ingresoEfectivo',
+	'cargaEfectiva',
+	'egresoEfectivo',
+	'conciliaciones',
+	'inventarioFinal',
+	'restan'
+];
+
+const consolidadoExtractedDataSchema = z.object({
+	categoria: z.string(),
+	funcionario: z.string(),
+	desde: z.coerce.date(),
+	hasta: z.coerce.date(),
+	inventarioInicial: z.number(),
+	ingresoEfectivo: z.number(),
+	cargaEfectiva: z.number(),
+	egresoEfectivo: z.number(),
+	conciliaciones: z.number(),
+	inventarioFinal: z.number(),
+	restan: z.number()
+});
+
+type WorkbookPage = {
+	name: string;
+	data: unknown[][];
+};
+
+type Workbook = Array<WorkbookPage>;
+
+const getFileData = async (file: File) => {
+	const woorkbook: Workbook = xlsx.parse(await file.arrayBuffer());
+	return woorkbook.map(extractWorkbookPageData);
+};
+
+const extractWorkbookPageData = (page: WorkbookPage) => {
+	const pageData = page.data
+		.map((data) => consolidadoRowSchema.safeParse(data))
+		.filter((parsed) => parsed.success)
+		.map((parsed) => parsed.data!.filter((value) => value !== undefined))
+		.map((data) =>
+			consolidadoExtractedDataSchema.parse(_.zipObject(consolidadoExtractedDataSchemaColumns, data))
+		);
+	return { name: page.name, data: pageData };
+};
+
+const aggregatePageDataOral = (
+	funcionario: string,
+	diasHabilesDespacho: number,
+	diasHabilesFuncionario: number,
+	data: Array<z.infer<typeof consolidadoExtractedDataSchema>>
+) => {
+	const firstFuncionarioRow = data.find((d) => d.funcionario === funcionario);
+	const minDate = firstFuncionarioRow?.desde;
+	const totalInventarioInicial = _.sumBy(data, (d) =>
+		dayjs(d.desde).isSame(minDate) ? d.inventarioInicial : 0
+	);
+	const ingresoEfectivo = _.sumBy(data, (d) => d.ingresoEfectivo);
+
+	const ingresoEfectivoUltimoPeriodo = _.sumBy(data, (d) =>
+		dayjs(d.desde).month() >= 9 &&
+		!['Incidentes de Desacato', 'Movimiento de Tutelas'].includes(d.categoria)
+			? d.ingresoEfectivo
+			: 0
+	);
+
+	const egresoFuncionario = _.sumBy(data, (d) =>
+		d.funcionario === funcionario ? d.egresoEfectivo : 0
+	);
+
+	const egresoOtrosFuncionarios = _.sumBy(data, (d) =>
+		dayjs(d.desde).month() < 9 && d.funcionario !== funcionario ? d.egresoEfectivo : 0
+	);
+
+	const cargaBaseCalificacionDespacho =
+		totalInventarioInicial + ingresoEfectivo - ingresoEfectivoUltimoPeriodo;
+	const cargaBaseCalificacionFuncionario = cargaBaseCalificacionDespacho - egresoOtrosFuncionarios;
+	const cargaProporcional =
+		(cargaBaseCalificacionDespacho * diasHabilesFuncionario) / diasHabilesDespacho;
+	const subfactorRespuestaEfectiva =
+		(Math.min(egresoFuncionario, cargaBaseCalificacionFuncionario) / cargaProporcional) * 40;
+
+	return {
+		totalInventarioInicial,
+		cargaBaseCalificacionDespacho,
+		cargaBaseCalificacionFuncionario,
+		egresoFuncionario,
+		cargaProporcional,
+		subfactorRespuestaEfectiva
+	};
+};
+
+const aggregatePageDataGarantias = (
+	funcionario: string,
+	diasHabilesDespacho: number,
+	diasHabilesFuncionario: number,
+	data: Array<z.infer<typeof consolidadoExtractedDataSchema>>
+) => {
+	const firstFuncionarioRow = data.find((d) => d.funcionario === funcionario);
+	const minDate = firstFuncionarioRow?.desde;
+	const totalInventarioInicial = _.sumBy(data, (d) =>
+		dayjs(d.desde).isSame(minDate) ? d.inventarioInicial : 0
+	);
+	const ingresoEfectivo = _.sumBy(data, (d) => d.ingresoEfectivo);
+
+	const egresoFuncionario = _.sumBy(data, (d) =>
+		d.funcionario === funcionario ? d.egresoEfectivo : 0
+	);
+
+	const egresoOtrosFuncionarios = _.sumBy(data, (d) =>
+		dayjs(d.desde).month() < 9 && d.funcionario !== funcionario ? d.egresoEfectivo : 0
+	);
+
+	const cargaBaseCalificacionDespacho = totalInventarioInicial + ingresoEfectivo;
+	const cargaBaseCalificacionFuncionario = cargaBaseCalificacionDespacho - egresoOtrosFuncionarios;
+	const cargaProporcional =
+		(cargaBaseCalificacionDespacho * diasHabilesFuncionario) / diasHabilesDespacho;
+	const subfactorGarantias = Math.min(
+		(Math.min(egresoFuncionario, cargaBaseCalificacionFuncionario) / cargaProporcional) * 45,
+		45
+	);
+
+	return {
+		totalInventarioInicial,
+		cargaBaseCalificacionDespacho,
+		cargaBaseCalificacionFuncionario,
+		egresoFuncionario,
+		cargaProporcional,
+		subfactorGarantias
+	};
+};
+
+export const actions = {
+	default: async ({ request }) => {
+		const data = await request.formData();
+		const file = data.get('file') as File;
+		const fileData = await getFileData(file);
+
+		const oralPage = fileData.find((page) => page.name === 'Oral');
+		const garantiasPage = fileData.find((page) => page.name === 'Garantias');
+		if (!oralPage || !garantiasPage) return { success: false };
+
+		// TODO: CALCULAR/SOLICITAR VALORES DE ESTAS CONSTANTES
+		const FUNCIONARIO = 'Funcionario: ADRIANA DEL PILAR ARENAS NIÑO Cédula: 46456640'; // 'Funcionario: FERNANDO IBAGUE  PINILLA Cédula: 7321266';
+		const TIPO_DESPACHO = 'Promiscuo Municipal';
+		const DIAS_HABILES_DESPACHO = 227; // DEPENDE DEL TIPO DE DESPACHO
+		const DIAS_HABILES_LABORADOS = 226; // 207; // TODO: REGISTRAR NOVEDADES CON DIAS DESCONTADOS
+		const AUDIENCIAS_PROGRAMADAS = 14; //60;
+		const AUDIENCIAS_ATENDIDAS = 9; // 60;
+		const AUDIENCIAS_APLAZADAS_CAUSAS_AJENAS = 5; //0;
+		const AUDIENCIAS_APLAZADAS_JUSTIFICADAS = 0;
+		const AUDIENCIAS_APLAZADAS_NO_JUSTIFICADAS = 0;
+
+		const oral = aggregatePageDataOral(
+			FUNCIONARIO,
+			DIAS_HABILES_DESPACHO,
+			DIAS_HABILES_LABORADOS,
+			oralPage.data
+		);
+
+		const garantias = aggregatePageDataGarantias(
+			FUNCIONARIO,
+			DIAS_HABILES_DESPACHO,
+			DIAS_HABILES_LABORADOS,
+			garantiasPage.data
+		);
+
+		const calificacionAudiencias =
+			((AUDIENCIAS_ATENDIDAS +
+				AUDIENCIAS_APLAZADAS_CAUSAS_AJENAS +
+				AUDIENCIAS_APLAZADAS_JUSTIFICADAS) /
+				AUDIENCIAS_PROGRAMADAS) *
+			5;
+
+		const factorEficienciaAudiencias = oral.subfactorRespuestaEfectiva + calificacionAudiencias;
+
+		const calificacionTotalFactorEficiencia =
+			(factorEficienciaAudiencias + garantias.subfactorGarantias) / 2;
+
+		return {
+			success: true,
+			oral,
+			garantias,
+			calificacionAudiencias,
+			factorEficienciaAudiencias,
+			calificacionTotalFactorEficiencia
+		};
+	}
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type {
+		// PageData,
+		ActionData
+	} from './$types';
+
+	// export let data: PageData;
+	// console.log(data);
+
+	export let form: ActionData;
+</script>
+
+<form method="post" enctype="multipart/form-data">
+	<input type="file" name="file" />
+	<button type="submit">Cargar</button>
+</form>
+
+{#if form?.success}
+	<h3>Oral</h3>
+	<p>Total inventario inicial: {form.oral?.totalInventarioInicial}</p>
+	<p>Carga base de calificación del despacho: {form.oral?.cargaBaseCalificacionDespacho}</p>
+	<p>Carga base de calificación del funcionario: {form.oral?.cargaBaseCalificacionFuncionario}</p>
+	<p>Egreso del funcionario: {form.oral?.egresoFuncionario}</p>
+	<p>Carga proporcional: {form.oral?.cargaProporcional?.toFixed(2)}</p>
+	<p>
+		Subfactor respuesta efectiva a la demanda de justicia:
+		{form.oral?.subfactorRespuestaEfectiva?.toFixed(2)}
+	</p>
+	<p>Calificación audiencias: {form.calificacionAudiencias}</p>
+	<p>Calificación final factor eficiencia: {form.factorEficienciaAudiencias?.toFixed(2)}</p>
+
+	<h3>Garantías</h3>
+	<p>Total inventario inicial: {form.garantias?.totalInventarioInicial}</p>
+	<p>Carga base de calificación del despacho: {form.garantias?.cargaBaseCalificacionDespacho}</p>
+	<p>
+		Carga base de calificación del funcionario: {form.garantias?.cargaBaseCalificacionFuncionario}
+	</p>
+	<p>Egreso del funcionario: {form.garantias?.egresoFuncionario}</p>
+	<p>Carga proporcional: {form.garantias?.cargaProporcional?.toFixed(2)}</p>
+	<p>Subfactor garantías: {form.garantias?.subfactorGarantias?.toFixed(2)}</p>
+
+	<h3>Calificación factor eficiencia o rendimiento</h3>
+	<h3>{form.calificacionTotalFactorEficiencia?.toFixed(2)}</h3>
+{/if}
