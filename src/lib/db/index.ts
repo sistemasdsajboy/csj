51d7c0c542389db5fa433d61991a458bb7eba846
Oscar Saraza <oscarsaraza@gmail.com>
2024-05-20 08:58:46 -0500

    Update db/index.ts to store records on a JSON file

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,17 +1,21 @@
 import type { GradeAccumulatedData } from '$lib/core/calificaciones';
+import fs from 'node:fs';
 
 export type RegistroPorCalificar = {
 	despacho: string;
 	data: GradeAccumulatedData[];
 	funcionarios: string[];
 };
-const records: RegistroPorCalificar[] = [];
+let records: RegistroPorCalificar[] = [];
 
 export async function getRegistrosPorCalificar() {
+	const data = fs.readFileSync('./static/records.json', 'utf8');
+	records = JSON.parse(data);
 	return records;
 }
 export async function createRegistroPorCalificar(registro: RegistroPorCalificar) {
 	records.push(registro);
+	fs.writeFileSync('./static/records.json', JSON.stringify(records));
 }
 
 export async function getRegistroPorCalificarByDespacho(despacho: string) {
