d146ff43f1502e7c12cdb48043d5b822ca6cdc04
Oscar Saraza <oscarsaraza@gmail.com>
2024-05-21 11:01:37 -0500

    Tailwind and shadcn-svelte added

--- src/routes/FileLoader.svelte
+++ src/routes/FileLoader.svelte
@@ -1,5 +1,10 @@
-<h3>Cargar archivo consolidado para calificar</h3>
+<script>
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
+</script>
+
+<h3 class="text-xl font-bold">Cargar archivo consolidado para calificar</h3>
 <form method="post" action="?/loadFile" enctype="multipart/form-data">
-	<input type="file" name="file" />
-	<button type="submit">Cargar</button>
+	<Input type="file" name="file" accept=".xlsx, .xls" />
+	<Button type="submit">Cargar</Button>
 </form>
