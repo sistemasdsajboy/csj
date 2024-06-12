a73513c6c07585f8e06438f99a559a260a3f8376
Oscar Saraza <oscarsaraza@gmail.com>
2024-06-12 11:20:19 -0500

    Asignación de calificación a despacho
    - Listado de calificaciones
    - Funcionalidad de asignación de calificaciones a un despacho calificador.
    
    Closes #4

--- src/routes/(loggedUser)/calificaciones/+page.server.ts
+++ src/routes/(loggedUser)/calificaciones/+page.server.ts
@@ -1,6 +1,57 @@
 import { db } from '$lib/db/client';
+import { error, fail, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load = (async () => {
-	return {};
+export const load = (async ({ locals }) => {
+	if (!locals.user) error(400, 'No autorizado');
+	const user = await db.user.findFirst({ where: { id: locals.user?.id } });
+
+	if (!user) error(400, 'Usuario no encontrado');
+	const { despachosSeccionalIds } = user;
+
+	const calificaciones = await db.calificacion.findMany({
+		where: {
+			OR: [
+				{ despachoSeccionalId: { in: despachosSeccionalIds } },
+				{ despachoSeccionalId: { isSet: false } }
+			]
+		},
+		select: {
+			id: true,
+			estado: true,
+			despacho: { select: { nombre: true } },
+			despachoSeccional: { select: { nombre: true } }
+		},
+		orderBy: { despacho: { nombre: 'asc' } }
+	});
+
+	const despachos = await db.despachoSeccional.findMany({
+		orderBy: { nombre: 'asc' }
+	});
+
+	return { calificaciones, despachos };
 }) satisfies PageServerLoad;
+
+export const actions = {
+	actualizarDespacho: async ({ request, locals }) => {
+		if (!locals.user) error(400, 'No autorizado');
+
+		const formData = await request.formData();
+		const calificacionId = formData.get('calificacionId') as string;
+
+		const calificacion = await db.calificacion.findFirst({
+			where: { id: calificacionId }
+		});
+		if (!calificacion) return fail(400, { error: 'Calificación no encontrada.' });
+
+		const despachoId = formData.get('despachoId') as string;
+		if (!despachoId) return fail(400, { error: 'Debe seleccionar un despacho' });
+
+		await db.calificacion.update({
+			where: { id: calificacionId },
+			data: { despachoSeccionalId: despachoId }
+		});
+
+		throw redirect(302, '/calificaciones');
+	}
+};
--- src/routes/(loggedUser)/calificaciones/+page.svelte
+++ src/routes/(loggedUser)/calificaciones/+page.svelte
@@ -1,10 +1,58 @@
 <script lang="ts">
 	import PageLayout from '$lib/components/custom/page-layout.svelte';
-	import type { PageData } from './$types';
+	import { Badge } from '$lib/components/ui/badge';
+	import { cn } from '$lib/utils/shadcn';
+	import { Edit2Icon } from 'lucide-svelte';
+	import AsignarDespachoForm from './asignar-despacho-form.svelte';
 
-	export let data: PageData;
+	const { data } = $props();
+	const { calificaciones, despachos } = data;
+
+	const colors = {
+		borrador: 'bg-slate-700',
+		revision: 'bg-amber-700',
+		aprobada: 'bg-teal-700'
+	};
+	const labels = {
+		borrador: 'Borrador',
+		revision: 'Para revisar',
+		aprobada: 'Aprobada'
+	};
 </script>
 
-<PageLayout username={data.user}>
-    Calificaciones
+{#snippet header()}
+	<h1 class="grow text-lg font-bold uppercase">Calificaciones</h1>
+{/snippet}
+
+<!-- TODO: Filtros por despacho y estado -->
+<!--{#snippet sidebar()}
+	<div class="max-w-md">
+	</div>
+{/snippet} -->
+
+<PageLayout {header} username={data.user}>
+	<div>
+		{#each calificaciones as calificacion}
+			<div class="flex flex-row items-center justify-between gap-2">
+				<div
+					class={cn(
+						'inline-flex w-20 justify-center rounded-sm text-xs text-slate-100',
+						colors[calificacion.estado]
+					)}
+				>
+					{labels[calificacion.estado]}
+				</div>
+				<a href="/calificacion/{calificacion.id}">{calificacion.despacho.nombre}</a>
+
+				<div class="grow"></div>
+
+				<Badge variant={calificacion.despachoSeccional?.nombre ? 'default' : 'secondary'}>
+					{calificacion.despachoSeccional?.nombre || 'Sin despacho asignado'}
+				</Badge>
+				<AsignarDespachoForm calificacionId={calificacion.id} {despachos}>
+					<Edit2Icon class="h-4 w-4" />
+				</AsignarDespachoForm>
+			</div>
+		{/each}
+	</div>
 </PageLayout>
