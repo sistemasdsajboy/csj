e610ffe43ad656b5928b003537e7f7d7afb65a49
Oscar Saraza <oscarsaraza@gmail.com>
2024-10-02 13:56:00 -0500

    Fix error when an invalid session cookie is found.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,20 +1,32 @@
 import { lucia } from '$lib/server/auth';
 import type { Handle } from '@sveltejs/kit';
+import type { Session } from 'lucia';
+import type { User } from 'lucia';
+
+type ValidationResult = { user: User; session: Session } | { user: null; session: null };
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const sessionId = event.cookies.get(lucia.sessionCookieName);
+
 	if (!sessionId) {
 		event.locals.user = null;
 		event.locals.session = null;
 		return resolve(event);
 	}
 
-	const { session, user } = await lucia.validateSession(sessionId);
+	let validationResult: ValidationResult = { user: null, session: null };
+	try {
+		// Handle case where an invalid session info exists. In that case the validation throws an error
+		validationResult = await lucia.validateSession(sessionId);
+	} catch (error) {}
+
+	const { session, user } = validationResult;
+
 	if (session && session.fresh) {
 		const sessionCookie = lucia.createSessionCookie(session.id);
 		event.cookies.set(sessionCookie.name, sessionCookie.value, {
 			path: '.',
-			...sessionCookie.attributes
+			...sessionCookie.attributes,
 		});
 	}
 
@@ -22,7 +34,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 		const sessionCookie = lucia.createBlankSessionCookie();
 		event.cookies.set(sessionCookie.name, sessionCookie.value, {
 			path: '.',
-			...sessionCookie.attributes
+			...sessionCookie.attributes,
 		});
 	}
 
